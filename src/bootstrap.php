037ca7a7635335f791a8a8578c2af80565b64cc2
refinedPath <208536365+RefinedPath@users.noreply.github.com>
2026-05-16 15:53:09 -0400

    Set UTF-8 multibyte encoding in bootstrap.php

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 // src/bootstrap.php
 
+/* Set internal character encoding to UTF-8 */
+mb_internal_encoding("UTF-8");
+
 require_once __DIR__ . '/config/Config.php';
 require_once __DIR__ . '/config/Database.php';
 require_once __DIR__ . '/models/Note.php';
